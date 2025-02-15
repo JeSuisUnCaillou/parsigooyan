0b4938a91f22099c79189bb288732a4f704aef8f
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-02-15 19:23:44 +0100

    add the nav footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavFooter from './components/NavFooter.vue'
 </script>
 
 <template>
   <RouterView class="content" />
+  <NavFooter />
 </template>
 
 <style scoped>
---
+++ src/components/NavFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="footer">
+    <div class="footer-content">
+      Any questions ? Contact me at:<br /><br />
+      <span class="adress">jaleh.daftarian@gmail.com</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: $bg-color;
+  color: $txt-color;
+  padding: 20px;
+  min-height: $footer-height;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer-content {
+  max-width: $content-width;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.adress {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+</style>
