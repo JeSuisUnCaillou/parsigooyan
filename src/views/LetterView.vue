c34bdf474a6f316ce587563a0de69e9295053b15
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-02-15 21:35:12 +0100

    get definitions of a letter

--- src/views/LetterView.vue
+++ src/views/LetterView.vue
@@ -1,8 +1,44 @@
 <template>
   <div>
-    coucou
+    <back-button position="top" />
+
+    <h1>{{letter}}</h1>
+
+    <!-- <definitions :definitions="definitions"/> -->
+     {{ definitions }}
+
+    <back-button position="bottom" v-if="store.definitions && store.definitions.length > 5"/>
   </div>
 </template>
 
 <script setup>
+import BackButton from '@/components/BackButton.vue'
+import { useDefinitionsStore } from '@/stores/definitions.js'
+import { useRoute } from 'vue-router'
+import { ref, onMounted } from 'vue'
+
+
+const store = useDefinitionsStore()
+const route = useRoute()
+const letter = route.params.letter
+const definitions = ref(null)
+
+onMounted(async () => {
+  definitions.value = await store.get_definitions_of_letter({ letter: letter })
+})
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+}
+
+.definitions-count {
+  margin-top: 20px;
+  text-align: center;
+  padding: 20px;
+  background-color: $red;
+  color: $txt-color;
+  font-size: 1.2rem;
+}
+</style>
