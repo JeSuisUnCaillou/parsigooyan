918f61ff8d611cba74fa78b2da67975dd1b2215a
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-02-17 11:49:03 +0100

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -9,6 +9,30 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg}'] },
+      includeAssets: ['/logo_bouquetin_150x150.png'],
+      manifest: {
+        name: 'Parsigooyan',
+        short_name: 'Parsigooyan',
+        description: 'The only pocket dictionary of foreign words in Farsi',
+        theme_color: '#363445',
+        icons: [
+          {
+            src: '/logo_bouquetin_150x150.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo_bouquetin_150x150.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
