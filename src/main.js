ca0c0a91a374ee7923f2e5af98221b76eb29616f
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-02-17 11:17:32 +0100

    add google analytics

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueGtag, { config: { id: "G-6ZTS9KR80W" } }, router)
 
 app.mount('#app')
