3bb6d17e5874eb1942b1dba57cfa22b0506beb4c
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-02-17 14:53:42 +0100

    test the back button

--- cypress/e2e/letters_pages.cy.js
+++ cypress/e2e/letters_pages.cy.js
@@ -1,6 +1,14 @@
 // https://on.cypress.io/api
 
 describe('Letters pages', () => {
+  describe('Clicking the back button', () => {
+    it('goes back to the home page', () => {
+      cy.visit('/letter/%D8%B0')
+      cy.contains('a', 'Back').click()
+      cy.url().should('eq', Cypress.config().baseUrl + '/')
+    })
+  })
+
   describe('Clicking on a letter in the home', () => {
     beforeEach(() => {
       cy.visit('/')
@@ -41,6 +49,27 @@ describe('Letters pages', () => {
 
     it("displays the letter's definitions", () => {
       cy.get('.definition').should('have.length', 21)
+    })
+
+    describe('Clicking on another letter in the menu', () => {
+      beforeEach(() => {
+        cy.get('.burger').click()
+        cy.get('.menu-container').within(() => {
+          cy.contains('a', 'ذ').click()
+        })
+      })
+
+      it('goes to the letter page', () => {
+        cy.url().should('include', '/letter/%D8%B0')
+      })
+  
+      it('displays the letter in title', () => {
+        cy.contains('h1', 'ذ')
+      })
+  
+      it("displays the letter's definitions", () => {
+        cy.get('.definition').should('have.length', 38)
+      })
     })
   })
 })
