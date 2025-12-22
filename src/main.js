addcdd241d803699f944122ab69cab1e97957a0a
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-12-22 15:50:30 +0100

    improve cache control

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { createGtag } from 'vue-gtag'
+import { registerSW } from 'virtual:pwa-register'
+
+// Register service worker with auto-update and reload
+registerSW({
+  onNeedRefresh() {
+    // New content is available, reload the page automatically
+    window.location.reload()
+  },
+  onOfflineReady() {
+    console.log('App is offline. It can\'t access the definitions database.')
+  },
+})
 
 const app = createApp(App)
 
