dec7eaa0ea1878f0cf76942ab2497f539a1652d2
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-12-22 13:31:19 +0100

    fade-in elements in documents

--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -2,27 +2,33 @@
 <script setup>
 import documentsData from '@/data/documents.json'
 import Document from '@/components/Document.vue'
+import BackButton from '@/components/BackButton.vue'
 
 const documents = documentsData
 </script>
 
 <template>
   <div class="content documents-container content-min-height">
-    <h1>
+    <back-button position="top" />
+
+    <h1 class="fade-in fade-in-1">
       نوشتار های زیر به زبان پارسی و بدون واژه های بیگانه
     </h1>
-    <p>
+    <p class="fade-in fade-in-2">
       همه این نوشتار ها به پارسی ناب نوشته شده‌اند.
 همه نمونه هایی روشن میباشند، در توانایی زبان ما ، و برای ما دشواری در خواندن ندارد.
     </p>
 
     <div class="documents">
       <Document 
-        v-for="document in documents"
+        :class="`fade-in fade-in-${index + 3}`"
+        v-for="(document, index) in documents"
         :key="document.title"
         :document="document"
       />
     </div>
+
+    <back-button  position="bottom" />
   </div>
 </template>
 
